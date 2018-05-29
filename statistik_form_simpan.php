57f523ee962d910a3e4d70e8ddbd7d09c8a20fed
Erwin <erwin@gti.co.id>
2018-05-29 08:37:42 +0700

    ok

--- statistik_form_simpan.php
+++ statistik_form_simpan.php
@@ -95,6 +95,28 @@
                         </td> 
                         </tr>  
 
+
+                        <!-- Awal Modal -->
+                      <div id="modal2<?php echo $data['id_pelayanan']; ?>" class="modal modal-fixed-footer">
+                          <div class="modal-content">
+                               <h4>Data Pelayanan Pasien Rumah Sakit</h4>
+                                <ul class="collection">
+                                  <li class="collection-item">Nama Lengkap Pasien : <?php echo $data['nama_lengkap']; ?></li>
+                                  <li class="collection-item">Nama Dokter Pemeriksa : <?php echo $data['nama_pegawai']; ?></li>
+                                  <li class="collection-item">Nama Kamar : <?php echo $data['nama_kamar']; ?></li>
+                                  <li class="collection-item">Keluha : <?php echo $data['keluhan']; ?></li>
+                                  <li class="collection-item">Diagnosa : <?php echo $data['diagnosa']; ?></li>
+                                  <li class="collection-item">Tanggal Masuk RS : <?php echo $data['tgl_masuk']; ?></li>
+                                  <li class="collection-item">Tanggal Keluar RS : <?php echo $data['tgl_keluar']; ?></li>
+                                  <li class="collection-item">Keterangan : <?php echo $data['keterangan']; ?></li>
+                                 </ul>
+                              </div>
+                                  <div class="modal-footer">
+                                    <a href="javascript:;" class="modal-action modal-close waves-effect waves-green btn-flat ">Tutup</a>
+                                  </div>
+                              </div>
+                             <!-- Akhir Modal --> 
+
                         <?php
                         }
                         ?>
