8e6fc1e89b075d28c0841c45f7e684c0d434bdef
Erwin <erwin@gti.co.id>
2018-05-29 22:21:02 +0700

    ok

--- config/footer.php
+++ config/footer.php
@@ -1,7 +1,7 @@
 <footer class="page-footer gradient-45deg-light-blue-cyan">
       <div class="footer-copyright">
         <div class="container">
-          <span class="right hide-on-small-only"> Farah Saleh Abdat : 14.63.00341</span>
+          <span class="right hide-on-small-only"> Farah Saleh Abdat : 14.63.0341</span>
         </div>
       </div>
     </footer>
