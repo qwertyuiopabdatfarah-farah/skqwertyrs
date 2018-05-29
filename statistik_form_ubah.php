3df73a7e1ab8e7df6719b99f3020719a8043b0a0
Erwin <erwin@gti.co.id>
2018-05-29 08:16:21 +0700

    Ok

--- statistik_form_ubah.php
+++ statistik_form_ubah.php
@@ -54,67 +54,23 @@
                         <thead>
                         <tr>
                         <th> No </th>
-                        <th> Nama Lengkap Pasien </th>
-                        <th> Nama Dokter Pemeriksa </th>
-                        <th> Tanggal Masuk </th>
-                        <th> Tanggal Keluar </th>
-                        <th style="width:100px;text-align: center;"> Aksi </th>
+                        <th> No Registrasi </th>
+                        <th> Nama Lengkap </th>
                         </tr>
                         </thead>
                        <tbody>
                         <?php
                         include 'config/koneksiDB.php';  
-                        $query = "SELECT pasien.nama_lengkap, 
-                                         pegawai.nama_pegawai, 
-                                         kamar.nama_kamar, 
-                                         pelayanan.id_pelayanan, 
-                                         pelayanan.keluhan, 
-                                         pelayanan.diagnosa, 
-                                         pelayanan.tgl_masuk,
-                                         pelayanan.tgl_keluar, 
-                                         pelayanan.keterangan 
-                                         FROM pelayanan 
-                                         LEFT JOIN pasien 
-                                         ON pasien.id_pasien = pelayanan.id_pasien
-                                         LEFT JOIN pegawai 
-                                         ON pegawai.id_pegawai = pelayanan.id_pegawai
-                                         LEFT JOIN kamar
-                                         ON Kamar.id_kamar = pelayanan.id_kamar";
+                        $query = "SELECT * pasien ORDER BY id_pasien DESC";
                         $result=mysqli_query($koneksi, $query) or die(mysqli_error());
                         $no=1; //penomoran 
                         while ($data = mysqli_fetch_array($result, MYSQLI_BOTH)){
                         ?>
                         <tr>
                         <td><?php echo $no++; ?></td>
-                        <td><?php echo $data['nama_lengkap']; ?></td>  
-                        <td><?php echo $data['nama_pegawai']; ?></td>
-                        <td><?php echo $data['tgl_masuk']; ?></td>
-                        <td><?php echo $data['tgl_keluar']; ?></td> 
-                        <td style="width:100px;text-align: center;">
-                        <a class='waves-effect waves-light modal-trigger' href='#modal2<?php echo $data['id_pelayanan']; ?>'>|Detail|</a>
-                        </td> 
+                        <td><?php echo $data['no_registrasi']; ?></td>
+                        <td><?php echo $data['nama_lengkap']; ?></td>   
                         </tr>  
-
-                      <!-- Awal Modal -->
-                      <div id="modal2<?php echo $data['id_pelayanan']; ?>" class="modal modal-fixed-footer">
-                          <div class="modal-content">
-                               <h4>Data Pelayanan Pasien Rumah Sakit</h4>
-                                <ul class="collection">
-                                  <li class="collection-item">Nama Lengkap Pasien : <?php echo $data['nama_lengkap']; ?></li>
-                                  <li class="collection-item">Nama Dokter Pemeriksa : <?php echo $data['nama_pegawai']; ?></li>
-                                  <li class="collection-item">Nama Kamar : <?php echo $data['nama_kamar']; ?></li>
-                                  <li class="collection-item">Keluha : <?php echo $data['keluhan']; ?></li>
-                                  <li class="collection-item">Diagnosa : <?php echo $data['diagnosa']; ?></li>
-                                  <li class="collection-item">Tanggal Masuk RS : <?php echo $data['tgl_masuk']; ?></li>
-                                  <li class="collection-item">Tanggal Keluar RS : <?php echo $data['tgl_keluar']; ?></li>
-                                  <li class="collection-item">Keterangan : <?php echo $data['keterangan']; ?></li>
-                                 </ul>
-                              </div>
-                                  <div class="modal-footer">
-                                    <a href="javascript:;" class="modal-action modal-close waves-effect waves-green btn-flat ">Tutup</a>
-                                  </div>
-                              </div>
-                             <!-- Akhir Modal --> 
                         <?php
                         }
                         ?>
